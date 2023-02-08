e3040af135235dbb7af124a0b7e262f97603cc88
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-08 12:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+ const button = document.querySelector("#button");
+  const url = document.querySelector("#url");
+
+  button.addEventListener("click", function(event) {
+    event.preventDefault();
+    const name = document.querySelector("#name").value;
+    const year = document.querySelector("#year").value;
+    url.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+  });
